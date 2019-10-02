b23ea1bd449a693b41cc1fef36611b8df5fb9a5d
Junpeng He <hejunpeng2012@hotmail.com>
2019-10-02 13:50:48 +0800

    sending post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -74,7 +75,24 @@ export default class BurgerBuilder extends Component {
     } 
 
     purchaseContinuedHandler = () => {
-        alert('You continue!')
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Junpeng',
+                address: {
+                    street: 'No.1 st.',
+                    zipCode: '213421',
+                    country: 'China'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
+            
     }
 
     render() {
