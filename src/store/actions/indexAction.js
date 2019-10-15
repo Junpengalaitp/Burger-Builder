621081865338ae7b83563f51236dcdfdd7569571
Junpeng He <hejunpeng2012@hotmail.com>
2019-10-15 21:06:01 +0800

    redirecting the user to the checkout page

--- src/store/actions/indexAction.js
+++ src/store/actions/indexAction.js
@@ -1,3 +1,3 @@
 export { addIngredient, removeIngredient, initIngredients } from './burgerBuilderActions'
 export { purchaseBurger, purchaseInit, fetchOrders } from './order'
-export { auth, logout } from './Auth'
+export { auth, logout, setAuthRedirectPath } from './Auth'
