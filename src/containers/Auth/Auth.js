b78ada88e39a65de296581c61bbf86cba15f415a
hejp009 <hejunpeng2012@hotmail.com>
2019-10-15 15:02:39 +0800

    add sign in

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,7 @@ import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.module.css'
 import * as actions from '../../store/actions/indexAction'
+
 class Auth extends Component {
     state = {
         controls: {
@@ -36,7 +37,8 @@ class Auth extends Component {
                 valid: false,
                 touched: false
             }
-        }
+        },
+        isSignUp: true
     }
 
     inputChangedHandler = (event, controlName) => {
@@ -79,8 +81,15 @@ class Auth extends Component {
 
     submitHandler = (event) => {
         event.preventDefault()
-        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value)
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignUp)
     }
+
+    switchAuthModeHandler = () => {
+        this.setState(prevState => {
+            return {isSignUp: !prevState.isSignUp}
+        })
+    }
+    
 
     render() {
         const formElementsArray = [];
@@ -109,6 +118,11 @@ class Auth extends Component {
                     {form}
                     <Button btnType='Success'>SUBMIT</Button>
                 </form>
+                <Button 
+                    clicked={this.switchAuthModeHandler}
+                    btnType='Danger'>
+                        SWITCH TO {this.state.isSignUp ? 'SIGN IN': 'SIGN UP'}
+                </Button>
             </div>
         )
     
@@ -117,7 +131,7 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password) => dispatch(actions.auth(email, password))
+        onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
     }
 }
 
--- src/store/actions/Auth.js
+++ src/store/actions/Auth.js
@@ -21,7 +21,7 @@ export const authFail = (error) => {
     }
 }
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignUp) => {
     return dispatch => {
         dispatch(authStart())
         const authData = {
@@ -29,7 +29,11 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         }
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBU14fK8m_Wibcsb1fUadz1Jy2tdt5ya0M', authData)
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBU14fK8m_Wibcsb1fUadz1Jy2tdt5ya0M'
+        if (!isSignUp) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBU14fK8m_Wibcsb1fUadz1Jy2tdt5ya0M'
+        }
+        axios.post(url, authData)
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data));
